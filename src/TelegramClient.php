7be25bc7297ef50c69438d6c652b0980b0978914
ttormt <ttormt@gmail.com>
2025-02-15 16:17:47 +0300

    Implement the photo downloading from telegram api

--- src/TelegramClient.php
+++ src/TelegramClient.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TtormtGptBot;
 
+use Exception;
 use TelegramBot\Api\Client;
 use TelegramBot\Api\Types\Message;
 use TelegramBot\Api\Types\Update;
@@ -18,6 +19,7 @@ class TelegramClient
 {
     private Client $bot;
     private OpenAiClient $openAi;
+    private const string API_LINK = 'https://api.telegram.org/file/bot' . TELEGRAM_BOT_SECRET . '/';
 
     public function __construct()
     {
@@ -62,8 +64,8 @@ class TelegramClient
     }
 
     /**
-    * Info on help command
-    */
+     * Info on help command
+     */
     public function help(Message $message)
     {
         $id = $message->getChat()->getId();
@@ -77,14 +79,43 @@ class TelegramClient
     {
         $message = $update->getMessage();
         $id = $message->getChat()->getId();
-        if (in_array($id, AUTHORIZED_IDS)) {
-            $this->bot->sendMessage($id, "You're authorized. Sending the message...");
-            $result = $this->openAi->sendMessage($message->getText());
-            $this->bot->sendMessage($id, "Got the response...");
-            $this->bot->sendMessage($id, $result, 'Markdown');
+        if (!in_array($id, AUTHORIZED_IDS)) {
+            $this->bot->sendMessage($id, "You aren't authorized. Sorry.");
             return;
         }
-        $this->bot->sendMessage($id, "You aren't authorized. Sorry.");
+        $this->bot->sendMessage($id, "You're authorized. Sending the message...");
+        if (!empty($photo = $message->getPhoto())) {
+            $photo = array_pop($photo);
+            $fileId = $photo->getFileId();
+            $file = $this->bot->getFile($fileId);
+            $downloadPath = self::API_LINK . $file->getFilePath();
+            $savedFilePath = $this->downloadFile($downloadPath);
+            return;
+        } else {
+            $result = $this->openAi->sendMessage($message->getText());
+        }
+        $this->bot->sendMessage($id, "Got the response...");
+        $this->bot->sendMessage($id, $result, 'Markdown');
+    }
+
+    /**
+     * Downloads a file and returns its path
+     */
+    private function downloadFile(string $path): string
+    {
+        $fileName = __DIR__ . '/../tmp/' . md5(uniqid()) . '.' . pathinfo($path, PATHINFO_EXTENSION);
+        $curl = curl_init($path);
+        $fileHandle = fopen($fileName, 'wb');
+
+        curl_setopt($curl, CURLOPT_FILE, $fileHandle);
+
+        $result = curl_exec($curl);
+        if ($result === false) {
+            throw new Exception('File download error ' . curl_error($curl));
+        }
+        curl_close($curl);
+        fclose($fileHandle);
+        return $fileName;
     }
 
     /**
